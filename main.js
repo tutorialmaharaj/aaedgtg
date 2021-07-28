355cfbed7e71749f931512011f2ad669cd53d4e7
Tutorial maharaj <70699370+tutorialmaharaj@users.noreply.github.com>
2021-07-28 13:31:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+
+var cnavas = fabric.Canvas("myCanvas");
+
+ball_y=0;
+ball_x=0;
+hole_y=400;
+hole_x=800;
+
+
+
+
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	fabrioc.Image.fromURL("golf-h1.png", function(Img)
+	{
+		hole_obj = Img;
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToWidth(50);
+		hole_obj.set({
+			top:hole_y,
+			left:hole_x
+		});
+		canvas.add(hole_obj)
+	};
+	new_image();
+}
+
+function new_image()
+{
+	fabric.Image.fromUrl("ball.png",function(Img){
+		ball_obj = Img;
+		ball_obj.scaleToWidth(50);
+	    ball_obj.scaleToHeight(50);
+		ball_obj.set({
+			top:bal_y,
+			left:ball_x
+		});
+		canvas.add(ball_obj);});
+	}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		// Write a code to move ball upward.
+	}
+
+	function down()
+	{
+		 // Write a code to move ball downward.
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+		}
+	}
+	
+}
+
